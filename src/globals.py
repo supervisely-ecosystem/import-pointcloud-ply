8b6decef26181e3d0fd25c9411a8f84d213d98bc
cxnt <pavelbartsits@gmail.com>
2022-10-14 14:40:14 +0400

    add import from agent

--- src/globals.py
+++ src/globals.py
@@ -3,12 +3,10 @@ import sys
 from distutils.util import strtobool
 
 import supervisely as sly
-from dotenv import load_dotenv
 from fastapi import FastAPI
 from supervisely.app.fastapi import create
 from supervisely.io.fs import mkdir
 
-# from supervisely.pointcloud.pointcloud import ALLOWED_POINTCLOUD_EXTENSIONS
 
 app_root_directory = os.path.dirname(os.getcwd())
 sys.path.append(app_root_directory)
@@ -17,6 +15,7 @@ print(f"App root directory: {app_root_directory}")
 sly.logger.info(f'PYTHONPATH={os.environ.get("PYTHONPATH", "")}')
 
 # order matters
+# from dotenv import load_dotenv
 # load_dotenv(os.path.join(app_root_directory, "secret_debug.env"))
 # load_dotenv(os.path.join(app_root_directory, "debug.env"))
 
@@ -30,11 +29,12 @@ TASK_ID = int(os.environ["TASK_ID"])
 TEAM_ID = int(os.environ["context.teamId"])
 WORKSPACE_ID = int(os.environ["context.workspaceId"])
 INPUT_PATH = os.environ.get("modal.state.slyFolder", None)
+if INPUT_PATH:
+    IS_ON_AGENT = api.file.is_on_agent(INPUT_PATH)
 REMOVE_SOURCE = bool(strtobool(os.getenv("modal.state.remove_source")))
 
 DEFAULT_DATASET_NAME = "ds0"
 ALLOWED_POINTCLOUD_EXTENSIONS = [".ply"]
-# ALLOWED_POINTCLOUD_EXTENSIONS = ALLOWED_POINTCLOUD_EXTENSIONS
 
 STORAGE_DIR = os.path.join(app_root_directory, "debug", "data", "storage_dir")
 mkdir(STORAGE_DIR, True)
--- src/main.py
+++ src/main.py
@@ -65,8 +65,7 @@ def import_pointcloud_ply(api: sly.Api, task_id: int):
                                  f"structure, for subdirectories and duplicated file names")
             continue
 
-    sly.fs.remove_dir(dir_=g.STORAGE_DIR)
-    if g.REMOVE_SOURCE:
+    if g.REMOVE_SOURCE and not g.IS_ON_AGENT:
         api.file.remove(team_id=g.TEAM_ID, path=g.INPUT_PATH)
         source_dir_name = g.INPUT_PATH.lstrip("/").rstrip("/")
         sly.logger.info(
